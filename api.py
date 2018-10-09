bcb2ca76b4dcb8eab6f42bc60e96671e711e6afa
Julia <jhlauer@umich.edu>
2018-10-08 23:06:25 -0400

    finished 10/08

--- api.py
+++ api.py
@@ -1,21 +1,15 @@
-import json
+import base64
 import requests
 
-ACESS_TOKEN = f9394bc6e461c4ef3c0951aeff127ef6
-CLIENT_ID = a82a96cdbbf8d5bee5def75ad79d82823b32169e
-CLIENT_SECRET = Fjj9/he0uiU1msr7R4Eb2O3YneZ7o5XsN09aqKHnII+XqC3RNxvTBgn3V5e6zjkJKYljHQzngI8uZn8+z9QechktamjywKbHT3Atdz65TgiVzoLVHduWxp4meQO6+I1c
-AUTHORIZE_URL = https://api.vimeo.com/oauth/authorize
+OAUTH_TOKEN = "f9394bc6e461c4ef3c0951aeff127ef6"
+CLIENT_ID = b"a82a96cdbbf8d5bee5def75ad79d82823b32169e"
+CLIENT_SECRET = b"Fjj9/he0uiU1msr7R4Eb2O3YneZ7o5XsN09aqKHnII+XqC3RNxvTBgn3V5e6zjkJKYljHQzngI8uZn8+z9QechktamjywKbHT3Atdz65TgiVzoLVHduWxp4meQO6+I1c"
+headers = {"Authorization": "basic " + str(base64.b64encode(CLIENT_ID + b":" + CLIENT_SECRET))}
+data = {"grant_type": "client_credentials", "scope": "public"}
+auth = requests.post("https://api.vimeo.com/oauth/authorize/client/", data=data, headers=headers)
+print(auth)
 
-def vimeo(query):
-    baseurl = "https://api.vimeo.com/oauth/authorize/client"
-
-    #not sure what this is supposed to do
-    authorization-header: "Authorization : basic " + base64(client_id + ":" + client_secret)
-
-    request = requests.get(baseurl, params = {
-        'response_type': "token",
-        'client_id': CLIENT_ID,
-        'grant_type': 'client_credentials'}
-    response = request.json()
-    return response 
-    
+# safe staff picks for Vimeo videos !!!
+baseurl = "https://api.vimeo.com//channels/staffpicks/videos?filter=content_rating&filter_content_rating=safe,unrated"
+response = requests.get(baseurl)
+print(response)
--- files-js/carousel.js
+++ files-js/carousel.js
@@ -1,63 +1,37 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
+import Slider from "react-slick";
 
-export default () => (
-  <Carousel autoPlay>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
-      <p className="legend">Legend 1</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
-      <p className="legend">Legend 2</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
-      <p className="legend">Legend 3</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" />
-      <p className="legend">Legend 4</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" />
-      <p className="legend">Legend 5</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg" />
-      <p className="legend">Legend 6</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg" />
-      <p className="legend">Legend 7</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg" />
-      <p className="legend">Legend 8</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" />
-      <p className="legend">Legend 9</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg" />
-      <p className="legend">Legend 10</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-11.jpg" />
-      <p className="legend">Legend 11</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-12.jpg" />
-      <p className="legend">Legend 12</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-13.jpg" />
-      <p className="legend">Legend 13</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-14.jpg" />
-      <p className="legend">Legend 14</p>
-    </div>
-  </Carousel>
-);
+class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider {...settings}>
+        <div>
+          <img>food6.png</img>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    );
+  }
+}
+
